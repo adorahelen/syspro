18f6ba874d0bab2a4f3baefae52c90b4b64521bf
KIM KANG MIN <adorahelenmin@naver.com>
2023-12-12 19:15:02 +0900

    SnSlab!!

---
+++ Chap11/prob7/jump1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <setjmp.h>
+
+void p1(), p2();
+jmp_buf env;
+
+int main()
+{
+ if (setjmp(env) != 0) {
+   printf("due to error, return and processing\n");
+   exit(0);
+ }
+
+ else printf("first pass\n");
+
+ p1();
+
+}
+
+ void p1()
+{
+  p2();
+}
+ void p2()
+{
+  int error;
+
+  error = 1;
+  if (error) {
+  printf("error \n");
+  longjmp(env, 1);
+  }
+}
+
+
